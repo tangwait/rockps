8353bfcae5c3ead1d7976eb097409f18440e468b
tangwai <tmichael11314@gmail.com>
2023-07-10 19:26:11 -0500

    rearrange determineWinner for consistency add loop to game() for 5 rounds add score user/computer ++ for each round add score variables and result declaration

--- script.js
+++ script.js
@@ -10,26 +10,46 @@ function getComputerChoice() {
         }
 
         if (userChoice === 'rock') {
-            if (computerChoice === 'paper')
-            return 'Computer picked ' + computerChoice + ' you lose!'; 
-        }   
-        
+            if (computerChoice === 'paper') {
+                computerScore++;
+                return 'Computer picked ' + computerChoice + ' you lose!'
+            }
+            userScore++;
+            return 'Computer picked ' + computerChoice + ' you win!';
+        }    
         if (userChoice === 'paper') {
-            if (computerChoice === 'scissors')
-            return 'Computer picked ' + computerChoice + ' you lose!';
-        }   
-        
-        if (userChoice === 'scissors') {
-            if (computerChoice === 'rock')
-            return 'Computer picked ' + computerChoice + ' you lose!';
+            if (computerChoice === 'scissors') {
+                computerScore++;
+                return 'Computer picked ' + computerChoice + ' you lose!';
+            }   
+            userScore++;
+            return 'Computer picked ' + computerChoice + ' you win!'
         }
-        return 'Computer picked ' + computerChoice + ' you win!';
+        if (userChoice === 'scissors') {
+            if (computerChoice === 'rock') {
+                computerScore++;
+                return 'Computer picked ' + computerChoice + ' you lose!';
+            }
+            userScore++;
+            return 'Computer picked ' + computerChoice + ' you win!'
+        }
     }
+    
     const game = () => {
-        let userChoice = prompt('pick rock, paper, scissors');
-        let computerChoice = getComputerChoice();
-        console.log('You played ' + userChoice);
+        for (let i = 0; i < 5; i++) {
+            let userChoice = prompt('pick rock, paper, scissors');
+            let computerChoice = getComputerChoice();
+            console.log('You played ' + userChoice);
+            console.log(determineWinner(userChoice, computerChoice));
+        }
+        if (userScore>computerScore)
+            return 'You win! ' + userScore + ' to ' + computerScore
+        if (userScore === computerScore)
+            return 'Tie game! ' + userScore + ' to ' + computerScore
+        if (userScore<computerScore)
+            return 'You lose! ' + userScore + ' to ' + computerScore
 
-        console.log(determineWinner(userChoice, computerChoice));
     };
 
+let userScore = 0;
+let computerScore = 0;
